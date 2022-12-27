6d8138db9c02af482b31ce39cf19e2fea6bc36bb
ssplot56 <ssplot56@gmail.com>
2022-12-27 12:23:42 +0200

    ready to deploy

--- src/main/java/com/splot/bot/model/User.java
+++ src/main/java/com/splot/bot/model/User.java
@@ -1,7 +1,7 @@
 package com.splot.bot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.sql.Timestamp;
 
 @Entity(name = "users")
